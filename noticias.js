41069b1f8a2905b9d3d1623892a813c86c1d789a
Isa Giongo <isagiongo@yahoo.com.br>
2018-12-08 17:10:38 -0200

    first commit

---
+++ noticias.js
@@ -0,0 +1,21 @@
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+
+    var categoria = req.url;
+    if (categoria == '/tecnologia') {
+        res.end("<html><body>Notícias sobre Tecnologia</body></html>");
+    } else if (categoria == '/moda') {
+        res.end("<html><body>Notícias sobre Moda</body></html>");
+    } else if (categoria == '/esportes') {
+        res.end("<html><body>Notícias sobre Esportes</body></html>");
+    } else {
+        res.end("<html><body>Portal de notícias</body></html>");
+    }
+
+});
+
+server.listen(3000);
+
+
+console.log('Criando um site de noticias com Node.');
