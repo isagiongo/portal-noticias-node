85f459bd090c9a0b4146c610d83212a065a71aeb
Isa Giongo <isagiongo@yahoo.com.br>
2018-12-12 19:21:53 -0200

    Adicionar página notícia

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -2,6 +2,7 @@ module.exports = function (app) {
 
     app.get('/noticias', function (req, res) {
         var connection = app.config.dbConnection();
+
         connection.query('select * from noticia', function (error, result) {
             res.render("noticias/noticias", { noticias: result });
         });
